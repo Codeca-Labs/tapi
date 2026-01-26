8c6a0ec1bad1be462f9cf075e6d5de0bde5e2da0
arch-schobeck <schobeck@archdocs.dev>
2026-01-26 02:03:01 -0500

    finished test automation(*6):     - added tapi_setup_tests, tapi_add_test, and tapi_run_tests, for adding and running tests from a local test suite.
        - added tapi_make_test to remove abstraction from the user as much as possible (still a lot of work that needs to be done).
    
        - added tapi_add_mock_to_test to also remove details from the user; simply allow the creation and adding of a mock to a test.
    
        - lots more to do in terms of stream capturing, but, for the most part a good portion of the mvp is done.

--- test/test.c
+++ test/test.c
@@ -1,3 +1,32 @@
+#include <tapi/tapi.h>
+#include <tapi/mock.h>
+
+int target_function(int x) {
+    return ~x + 1u;
+}
+
+int function() {
+    int result1 = 0x10;
+    int result2 = -0x1;
+    int result3 = result1 + result2;
+    int result4 = target_function(result3);
+    return result4 + 1u;
+}
+
+tapi_mock_return(tested_function_target, int, 0u);
+
+e_tapi_test_result_t test_function() {
+    /* act & assert. */
+    int result = function();
+    tapi_assert(result == 0u);
+    return E_TAPI_TEST_RESULT_PASSED;
+};
+
 int main(int argc, char** argv) {
+    tapi_test_t* test = tapi_make_test("test_function", test_function);
+    tapi_add_mock_to_test(test, function, target_function, tested_function_target);
+    tapi_test_t* tests[] = { test };
+    tapi_setup_tests(tests, 1u);
+    tapi_run_tests();
     return 0;
 }
